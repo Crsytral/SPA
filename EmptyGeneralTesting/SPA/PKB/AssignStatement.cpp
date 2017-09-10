b2bc3a3b15fb964c1205b4d869b3f5e66d1d8545
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-09-10 15:36:50 +0800

    Implement AssignStatement

--- EmptyGeneralTesting/SPA/PKB/AssignStatement.cpp
+++ EmptyGeneralTesting/SPA/PKB/AssignStatement.cpp
@@ -1,8 +1,16 @@
 #include "AssignStatement.h"
 
-AssignStatement::AssignStatement(int index, Variable* v, vector<Variable*>* usedVars, Expression* exp) {
-	stmtNo = index;
-	varModified = v;
-	varUsed = usedVars;
+AssignStatement::AssignStatement(int index, StatementContainer parent, Statement follows, Variable* v, vector<Variable>* usedVars, Expression* exp) {
+	Statement(index,&parent,&follows);
+	addModVar(*v);
+	useVar = *(new vector<Variable>(*usedVars));
 	this->exp = exp;
+}
+
+bool AssignStatement::isExpEqual(Expression exp) {
+	return *(this->exp) == exp;
+}
+
+bool AssignStatement::isContainExp(Expression exp) {
+	return (this->exp)->contains(exp);
 }
