d3022b105de40d8c632da81a93752fedd0591c0c
LookRain <brantscube@hotmail.com>
2017-09-11 00:30:08 +0800

    change token base class

--- EmptyGeneralTesting/SPA/Parser/Token.h
+++ EmptyGeneralTesting/SPA/Parser/Token.h
@@ -6,13 +6,20 @@ using namespace std;
 class Token
 {
 
-private:
+protected:
 	string type;
 	string value;
 public:
-	Token(string type, string value);
+	Token(string value);
+	Token();
 	string getType();
 	string getValue();
 	void print();
+
+	bool isNum();
+	bool isVar();
+	bool isOperand();
+	bool isOperator();
+
 
 };
