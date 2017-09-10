c1d5449078d7b843611d85bd30bde99dd349ecfa
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-09-10 11:43:31 +0800

    Implement getAllStatement() API for the statementList Implement isChild(int stmtNo) API for statementList

--- EmptyGeneralTesting/SPA/PKB/StatementList.cpp
+++ EmptyGeneralTesting/SPA/PKB/StatementList.cpp
@@ -7,6 +7,7 @@ StatementList::StatementList(StatementContainer* p) {
 
 void StatementList::addStatement(int stmtNo) {
 	Statement* stmt = new Statement(stmtNo, parentContainer, curr);
+	statementList.push_back(*stmt);
 	if(curr != nullptr)
 		curr->setFollowedBy(stmt);
 	curr = stmt;
@@ -16,19 +17,23 @@ StatementContainer* StatementList::getParentContainer() {
 	return parentContainer;
 }
 
-Statement* StatementList::getCurr() {
-	return curr;
-}
-
 Statement* StatementList::addAssignStatement(int index, Variable* v, vector<Variable*>* usedVars, Expression* exp) {
-	//To be implemented by Shermine
-	//I put nullptr here first to be able to build
+	
 	return nullptr;
 }
 
 StatementContainer* StatementList::addWhileStatement(int index, Variable* controlVar) {
-	//To be implemented by Shermine
-	//I put nullptr here first to be able to build
+	
 	return nullptr;
 }
 
+vector<Statement> StatementList::getAllStatement() {
+	return statementList;
+}
+
+bool StatementList::isChild(int stmtNo) {
+	Statement stmt(stmtNo, nullptr, nullptr);
+	vector<Statement>::iterator it = find(statementList.begin(),statementList.end(),stmt);
+
+	return it != statementList.end();
+}
