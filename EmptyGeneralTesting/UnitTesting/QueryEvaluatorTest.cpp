5304b4c0c594fb53da6b2070d3a635e3824f08ec
Crsytral <shouyu2@hotmail.com>
2017-09-12 00:10:13 +0800

    Update UnitTest for QueryEvaluator

--- EmptyGeneralTesting/UnitTesting/QueryEvaluatorTest.cpp
+++ EmptyGeneralTesting/UnitTesting/QueryEvaluatorTest.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../SPA/QPS/QueryEvaluator.h"
 #include "../SPA/QPS/QueryObject.h"
+#include "../SPA/QPS/QueryParser.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,8 +14,14 @@ namespace UnitTesting
 
 		TEST_METHOD(testConstructor)
 		{
-			QueryObject query;
-			QueryEvaluator qe(query);
+			QueryParser* qp;
+			qp = new QueryParser();
+			QueryObject queryObj = qp->getQueryObj("assign a, statement s, variable v; Select s such that follows(1,2)");
+
+			//Need a demo PKB 
+
+
+			QueryEvaluator qe(queryObj);
 			Result result = qe.getRawResult();
 			string s = result.toString();
 			Assert::AreEqual(s, (std::string)"");
